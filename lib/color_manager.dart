ad9da8dab8ff15820f06ea1b03fc22c0ff181ce6
MaFiSoft <mafi82290@online.de>
2025-05-15 12:07:02 +0200

    Add files via upload

---
+++ lib/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color blue = Colors.blue;
+  static Color red = Colors.red;
+  static Color green = Colors.green;
+  static Color yellow = Colors.yellow;
+}
